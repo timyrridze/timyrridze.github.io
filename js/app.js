36cc9d4ed41f6373b5b24a82172359225690332e
timyrridze <118799376+timyrridze@users.noreply.github.com>
2022-11-30 13:06:14 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 "use strict";
 
+localStorage.clear()
+
 import "../css/style.css"
 import { todoListState } from "./components/_todo-list.js";
 import { dateState } from "./components/_date.js";
 import { preloaderState } from "./components/_preloader.js";
 
-// localStorage.clear()
+
 
 
